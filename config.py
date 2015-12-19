c982b345d1814b05e40e46ac2de1dc526c0c24d7
Mike Liu <mikelkl@foxmail.com>
2015-12-19 22:30:54 +0800

    fixed bug

--- config.py
+++ config.py
@@ -6,5 +6,5 @@ SECRET_KEY = 'you-will-never-guess'
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
 
-UPLOAD_FOLDER = 'app/static/uploads'
+UPLOAD_FOLDER = 'app/static/uploads/avatars'
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
