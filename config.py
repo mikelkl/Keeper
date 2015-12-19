fe77e1066672f301e249c879818d93b0ed53ae1d
Mike Liu <mikelkl@foxmail.com>
2015-12-19 20:06:45 +0800

    upload avatar

--- config.py
+++ config.py
@@ -5,3 +5,6 @@ SECRET_KEY = 'you-will-never-guess'
 
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
+
+UPLOAD_FOLDER = 'app/static/uploads'
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
