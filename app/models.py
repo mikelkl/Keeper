e9a8101999b0763e8e3d2d31d383ddab2e8fff9d
Mike Liu <mikelkl@foxmail.com>
2015-12-20 14:51:41 +0800

    add email & log

--- app/models.py
+++ app/models.py
@@ -3,7 +3,7 @@ from app import db
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    nickname = db.Column(db.String(64), index=True, unique=True)
+    nickname = db.Column(db.String(64), index=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password = db.Column(db.String(20), index=True, unique=False)
     posts = db.relationship('Post', backref='author', lazy='dynamic')
