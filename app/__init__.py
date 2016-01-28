5497d872ff26e0a74364a4dc05986f6d514f1fc5
Mike Liu <mikelkl@foxmail.com>
2016-01-28 20:53:13 +0800

    comment email

--- app/__init__.py
+++ app/__init__.py
@@ -14,25 +14,25 @@ lm = LoginManager()
 lm.init_app(app)
 lm.login_view = 'login'
 
-if not app.debug:
-    import logging
+# if not app.debug:
+#     import logging
 
-    from logging.handlers import RotatingFileHandler
-    file_handler = RotatingFileHandler(
-        'tmp/Keeper.log', 'a', 1 * 1024 * 1024, 10)
-    file_handler.setFormatter(logging.Formatter(
-        '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
-    app.logger.setLevel(logging.INFO)
-    app.logger.addHandler(file_handler)
-    app.logger.info('Keeper startup')
+#     from logging.handlers import RotatingFileHandler
+#     file_handler = RotatingFileHandler(
+#         'tmp/Keeper.log', 'a', 1 * 1024 * 1024, 10)
+#     file_handler.setFormatter(logging.Formatter(
+#         '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+#     app.logger.setLevel(logging.INFO)
+#     app.logger.addHandler(file_handler)
+#     app.logger.info('Keeper startup')
 
-    from logging.handlers import SMTPHandler
-    credentials = None
-    if MAIL_USERNAME or MAIL_PASSWORD:
-        credentials = (MAIL_USERNAME, MAIL_PASSWORD)
-    mail_handler = SMTPHandler(
-        (MAIL_SERVER, MAIL_PORT), 'no-reply@' + MAIL_SERVER, ADMINS, 'Keeper failure', credentials)
-    mail_handler.setLevel(logging.ERROR)
-    app.logger.addHandler(mail_handler)
+#     from logging.handlers import SMTPHandler
+#     credentials = None
+#     if MAIL_USERNAME or MAIL_PASSWORD:
+#         credentials = (MAIL_USERNAME, MAIL_PASSWORD)
+#     mail_handler = SMTPHandler(
+#         (MAIL_SERVER, MAIL_PORT), 'no-reply@' + MAIL_SERVER, ADMINS, 'Keeper failure', credentials)
+#     mail_handler.setLevel(logging.ERROR)
+#     app.logger.addHandler(mail_handler)
 
 from app import views, models
