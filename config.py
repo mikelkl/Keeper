87462b8a9599986e9ee3e7e5b577527fb9914c3f
Mike Liu <mikelkl@foxmail.com>
2016-02-29 16:49:20 +0800

    修改配置选项

--- config.py
+++ config.py
@@ -2,11 +2,45 @@
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-CSRF_ENABLED = True
-SECRET_KEY = 'you-will-never-guess'
 
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
-SQLALCHEMY_COMMIT_ON_TEARDOWN = True
+# CSRF_ENABLED = True
+# SECRET_KEY = 'you-will-never-guess'
+#
+# SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+# SQLALCHEMY_COMMIT_ON_TEARDOWN = True
 
-UPLOAD_FOLDER = 'app/static/uploads/avatars'
-ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
+
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    SQLALCHEMY_COMMIT_ON_TEARDOWN = True
+    UPLOAD_FOLDER = 'app/static/uploads/avatars'
+    ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
+
+    @staticmethod
+    def init_app(app):
+        pass
+
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or \
+                              'sqlite:///' + os.path.join(basedir, 'data-dev.sqlite')
+
+
+class TestingConfig(Config):
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('TEST_DATABASE_URL') or \
+                              'sqlite:///' + os.path.join(basedir, 'data-test.sqlite')
+
+
+class ProductionConfig(Config):
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+                              'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+
+
+config = {
+    'development': DevelopmentConfig,
+    'testing': TestingConfig,
+    'production': ProductionConfig,
+    'default': DevelopmentConfig
+}
