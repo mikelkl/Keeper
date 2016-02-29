26d99ceaf45d34786a8d75ffa0a813b1e29893b4
Mike Liu <mikelkl@foxmail.com>
2016-02-29 15:05:56 +0800

    集成Python shell

--- run.py
+++ run.py
@@ -1,5 +1,19 @@
 #!flask/bin/python
 
-from app import manager
+from app import app, db
+from app.models import User, ECG, Treatment, Post
 
-manager.run()
+from flask.ext.script import Manager, Shell
+
+manager = Manager(app)
+
+
+def make_shell_context():
+    return dict(app=app, db=db, User=User, ECG=ECG,
+                Treatment=Treatment, Post=Post)
+
+
+manager.add_command('shell', Shell(make_context=make_shell_context))
+
+if __name__ == '__main__':
+    manager.run()
