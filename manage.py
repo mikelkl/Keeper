3ceaac0d7d2d664fdf1077a8eb355c6411bb98b1
Mike Liu <mikelkl@foxmail.com>
2016-03-10 14:31:38 +0800

    add deployment command

--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@ import os
 from app import create_app, db
 from app.models import User, ECG, Treatment, Post
 
-from flask.ext.migrate import Migrate, MigrateCommand
+from flask.ext.migrate import Migrate, MigrateCommand, upgrade
 from flask.ext.script import Manager, Shell
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
@@ -26,6 +26,14 @@ def test():
     import unittest
     tests = unittest.TestLoader().discover('tests')
     unittest.TextTestRunner(verbosity=2).run(tests)
+
+
+@manager._commands
+def deploy():
+    """Run deployment tests."""
+
+    # 把数据库迁移到最新修订版本
+    upgrade()
 
 
 if __name__ == '__main__':
