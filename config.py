e0166b90cbb399942f46e95d4fdf7f6e206e3777
Mike Liu <mikelkl@foxmail.com>
2016-03-16 15:13:57 +0800

    config log

--- config.py
+++ config.py
@@ -36,9 +36,24 @@ class ProductionConfig(Config):
                               'sqlite:///' + os.path.join(basedir, 'data.sqlite')
 
 
+class UnixConfig(ProductionConfig):
+    @classmethod
+    def init_app(cls, app):
+        ProductionConfig.init_app(app)
+
+        # log to syslog
+        import logging
+        from logging.handlers import SysLogHandler
+        syslog_handler = SysLogHandler()
+        syslog_handler.setLevel(logging.WARNING)
+        app.logger.addHandler(syslog_handler)
+
+
 config = {
     'development': DevelopmentConfig,
     'testing': TestingConfig,
     'production': ProductionConfig,
+    'unix': UnixConfig,
+
     'default': DevelopmentConfig
 }
